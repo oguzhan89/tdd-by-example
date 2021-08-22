f1641ea752a9576088deb580d2faec7b6c5d2b6b
Oguzhan Dombayci <oguzhan.dombayci@gmail.com>
2021-08-22 08:38:33 +0300

    #Chapter-3 TDD-12 Addition, finally

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,7 +3,7 @@ package guru.springframework;
 import lombok.ToString;
 
 @ToString
-public class Money {
+public class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -29,5 +29,9 @@ public class Money {
 
     public Money times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
