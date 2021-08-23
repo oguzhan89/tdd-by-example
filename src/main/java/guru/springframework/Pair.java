2bf573d72f7fcfc5518823e6af91f5972656b6bc
Oguzhan Dombayci <oguzhan.dombayci@gmail.com>
2021-08-23 08:14:05 +0300

    #Chapter-3 TDD-14 Change

---
+++ src/main/java/guru/springframework/Pair.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Pair {
+    private final String from;
+    private final String to;
+}
